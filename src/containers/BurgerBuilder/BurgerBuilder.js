2ef7e10dab07b4a4705320b64e191246eb6ab19d
Abdelrahman <abdelrahman202022@gmail.com>
2020-08-08 23:10:55 +0200

    reset state of ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,10 +11,10 @@ const INGREDIENTS_PRICES = {
 class BurgerBuilder extends React.Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
     },
     price: 4,
   };

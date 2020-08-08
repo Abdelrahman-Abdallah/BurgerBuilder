54b31b3530bcf5e17b856f7afb2b4bcedc5c41fb
Abdelrahman <abdelrahman202022@gmail.com>
2020-08-08 12:54:59 +0200

    dynamically render ingredients in burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,23 @@ import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 const burger = (props) => {
+  //we have ingredient HERE as prop
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    (ingredientKey) => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient
+            key={`${ingredientKey}+${i}`}
+            type={ingredientKey}
+          ></BurgerIngredient>
+        );
+      });
+    }
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bacon" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );

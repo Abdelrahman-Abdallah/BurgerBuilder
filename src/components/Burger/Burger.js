bcf434128de3d4b7542dbfd2e2feb15768a5b2fa
Abdelrahman <abdelrahman202022@gmail.com>
2020-08-08 15:08:26 +0200

    simple tweaks for burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,18 +3,27 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 const burger = (props) => {
   //we have ingredient HERE as prop
-  const transformedIngredients = Object.keys(props.ingredients).map(
+  let transformedIngredients = Object.keys(props.ingredients).map(
     (ingredientKey) => {
-      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
-        return (
-          <BurgerIngredient
-            key={`${ingredientKey}+${i}`}
-            type={ingredientKey}
-          ></BurgerIngredient>
-        );
-      });
+      return [...Array(props.ingredients[ingredientKey])]
+        .map((_, i) => {
+          return (
+            <BurgerIngredient
+              key={`${ingredientKey}+${i}`}
+              type={ingredientKey}
+            ></BurgerIngredient>
+          );
+        })
+        .reduce((arr, ele) => {
+          console.log(ele);
+          return arr.concat(ele);
+        }, []);
     }
   );
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = "<p>Please Start Adding ingredients</p>";
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />

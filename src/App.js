43e272cf16a70f62ea4ceed91e1a81ed6ef1263d
Abdelrahman Abdallah <abdelrahman202022@gmail.com>
2020-08-23 17:07:55 +0200

    add checkout and checkoutSummary preparing for routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 function App() {
   return (

40e7703336b2f034a18f1c67946352f74e6c33ff
Abdelrahman Abdallah <abdelrahman202022@gmail.com>
2020-08-27 15:15:43 +0200

    fetching order from server

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,14 +1,40 @@
 import React from "react";
 import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import WithError from "../../hoc/withErrorHandler/withErrorhandler";
 class Orders extends React.Component {
+  state = {
+    orders: null,
+    loading: true,
+  };
+  componentDidMount() {
+    axios.get("/orders.json").then((res) => {
+      let orders = [];
+      for (let key in res.data) {
+        orders.push({ ...res.data[key], id: key });
+      }
+      this.setState((prev) => ({ orders: orders, loading: false }));
+    });
+  }
   render() {
+    let orders = null;
+    if (this.state.orders) {
+      orders = this.state.orders.map((order) => (
+        <Order
+          key={order.id}
+          ingredients={order.ingredients}
+          price={+order.price}
+        />
+      ));
+    }
+
     return (
       <div>
-        <Order />
-        <Order />
-        <Order />
+        {this.state.loading ? <Spinner /> : null}
+        {orders}
       </div>
     );
   }
 }
-export default Orders;
+export default WithError(Orders, axios);

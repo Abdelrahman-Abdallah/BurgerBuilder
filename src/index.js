6806b87573a7913385411fa62170cfa268b297e2
Abdelrahman Abdallah <abdelrahman202022@gmail.com>
2020-08-30 10:36:21 +0200

    connect store to the application

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+// REACT REDUX CONFIGURATION
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
+const store = createStore(reducer);
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(
   <React.StrictMode>{app}</React.StrictMode>,
